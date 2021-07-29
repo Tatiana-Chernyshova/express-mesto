7bb55b5d5d6bf15caa58e13b10ff7c96d0bf1e5a
Vladimir Chernyshov <Tram27mrld@yandex.ru>
2021-07-29 12:38:45 +0300

    29/07/21 work

--- app.js
+++ app.js
@@ -10,15 +10,22 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useFindAndModify: false
 });
 
-const Cat = mongoose.model('Cat', { name: String });
+// const Cat = mongoose.model('Cat', { name: String });
 
-const kitty = new Cat({ name: 'Zildjian' });
-kitty.save().then(() => console.log('meow'));
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+app.use((req, res, next) => {
+  req.user = {
+    _id: '5d8b8592978f8bd833ca8133' // вставьте сюда _id созданного в предыдущем пункте пользователя
+  };
 
+  next();
+}); 
 app.use(express.static(path.resolve(__dirname, 'public')));
 // app.use(express.static(path.join('/index.html', 'public')));
 app.use(express.json());
 app.use('/users', rootRouter);
+
 
 app.listen(3000, () => {
   console.log('zapysk!!!')
--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,10 @@
 const fs = require('fs').promises;
+const User = require('../models/user');
 
 const getUsers = (req, res) => {
-  fs.readFile('./data.json')
+  // User.find({ name, about })
+  User.find({})
+  // fs.readFile('./data.json')
     .then(users => {
       res.status(200).send({data: JSON.parse(users)})
   })
@@ -15,16 +18,52 @@ const getUser = (req, res) => {
     // res.status(200).send(req.params.userId)
   })}
 const createUser = (req, res) => {
-  console.log(req.body)
+  const { name, about, avatar } = req.body; // получим из объекта запроса имя и описание пользователя
+  User.create({ name, about, avatar })
+    // вернём записанные в базу данные
+    .then(user => res.send({ data: user }))
+    // данные не записались, вернём ошибку
+    .catch(err => res.status(500).send({ message: 'Произошла ошибка' }));
+
+  // console.log(req.body)
   // fs.readFile('./data.json')
-  fs.readFile('./data.json')
-  .then(users => {
-    const users2 = JSON.parse(users);
-    users2.push(req.body)
-    fs.writeFile('./data.json', JSON.stringify(users2))
+  // fs.readFile('./data.json')
+  // .then(users => {
+    // const users2 = JSON.parse(users);
+    // users2.push(req.body)
+    // fs.writeFile('./data.json', JSON.stringify(users2))
     // if (!user) return res.status(404).send({message: 'takogo nema'})
     // res.status(200).send(user)
-  })}
+}; 
+
+PATCH /users/me — обновляет профиль
+const updateUser (req, res) => {
+  // обновим имя найденного по _id пользователя
+  User.findByIdAndUpdate(
+    req.params.id,
+    { name: 'Виктор Гусев' },
+    // Передадим объект опций:
+    {
+        new: true, // обработчик then получит на вход обновлённую запись
+        runValidators: true, // данные будут валидированы перед изменением
+        upsert: true // если пользователь не найден, он будет создан
+    }
+    )
+  .then(user => res.send({ data: user }))
+  .catch(user => res.send({ "Данные не прошли валидацию. Либо произошло что-то совсем немыслимое" })); ;
+
+
+
+PATCH /users/me/avatar — обновляет аватар
+
+
+// const User = require('../models/user');
+
+// router.post('/', (req, res) => {
+//   const { name, about } = req.body; // получим из объекта запроса имя и описание пользователя
+
+//   User.create({ name, about }); // создадим документ на основе пришедших данных
+// });
 
 
 module.exports = {
